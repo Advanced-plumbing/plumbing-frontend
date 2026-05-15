5580a2bf59987f5ee6f41e91f5cdf9594451707b
Diego <diegotasa35@hotmail.com>
2026-05-15 18:57:53 -0500

    Update AboutUsContainer.tsx

--- src/features/AboutUs/AboutUsContainer.tsx
+++ src/features/AboutUs/AboutUsContainer.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React from 'react';
-import {ContactUsToday} from "@/shared/components/ContactUsToday/ContactUsToday";
-import {Accreditations} from "@/shared/components/Accreditations/Accreditations";
 import ContactBlue from "@/shared/components/ContactBlue/ContactBlue";
 import {CompanyOverview} from "@/features/AboutUs/components/CompanyOverview/CompanyOverview";
 import {CoreValues} from "@/features/AboutUs/components/CoreValues/CoreValues";
